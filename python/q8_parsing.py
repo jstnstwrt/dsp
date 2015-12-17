76a1797c78c927bc161eb77524db0634a49d0a9c
Justin Stewart <justinstewart.la@gmail.com>
2015-12-17 14:43:50 -0500

    finished parsing challenge

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -1,19 +1,16 @@
-#The football.csv file contains the results from the English Premier League. 
+# -*- coding: utf-8 -*-
+# The football.csv file contains the results from the English Premier League. 
 # The columns labeled ‘Goals’ and ‘Goals Allowed’ contain the total number of 
-# goals scored for and against each team in that season (so Arsenal scored 79 goals 
-# against opponents, and had 36 goals scored against them). Write a program to read the file, 
-# then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
+# goals scored for and against each team in that season (so Arsenal scored 79
+# goals against opponents, and had 36 goals scored against them). Write a 
+# program to read the file, then print the name of the team with the smallest
+# difference in ‘for’ and ‘against’ goals. The below skeleton is optional. You
+# can use it or you can write the script with an approach of your choice.
 
-# The below skeleton is optional.  You can use it or you can write the script with an approach of your choice.
 
 
-import csv
+import pandas as pd
 
-  def read_data(data):
-   # COMPLETE THIS FUNCTION
-
-  def get_min_score_difference(self, parsed_data):
-    # COMPLETE THIS FUNCTION
-
-  def get_team(self, index_value, parsed_data):
-    # COMPLETE THIS FUNCTION
+df = pd.read_csv('football.csv')
+min_goal_diff_id = abs(df['Goals'] - df['Goals Allowed']).idxmin()
+print df['Team'][min_goal_diff_id]
