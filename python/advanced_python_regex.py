aaf65f0b1237f4d39891e2372cb19faaca068064
Justin Stewart <justinstewart.la@gmail.com>
2015-12-17 15:59:06 -0500

    finished first q of regex section

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,15 @@
-PLACE YOUR CODE HERE
+import pandas as pd
+import re
+
+df = pd.read_csv('faculty.csv')
+
+df.columns = [col.strip() for col in df.columns] #Fixing column names
+
+degree_strings = list(df['degree'].values)
+# Cleaning the entries so that they are uniform
+degree_lists = [d.strip().split(' ') for d in degree_strings]
+degrees = [degree.replace('.','').upper() for sublist in degree_lists\
+										  for degree in sublist]
+del degrees[degrees.index('0')] # Removing the non-degree from list
+frequencies =  pd.Series(degrees).value_counts()
+print frequencies
