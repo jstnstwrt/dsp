47067c43ba842c51f0b9a340866e13a0dbbaf584
Justin Stewart <justinstewart.la@gmail.com>
2015-12-17 18:14:26 -0500

    solved first part of the dictionary section

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,29 @@
-PLACE YOUR CODE HERE
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('faculty.csv')
+df.columns = [col.strip() for col in df.columns] #Fixing column names
+
+# Getting last names for dict key
+keys = df['name'].values.tolist()
+names = [fullname.strip().split(' ') for fullname in keys]
+last_names = [name[-1] for name in names]
+
+# Fixing titles to be correct form
+df['title'] = df['title'].map(lambda x: x.strip(' of Biostatistics'))
+
+
+# Creating dict
+vals = df[['degree','title','email']].values.tolist()
+
+faculty_dict = {}
+for key, val in zip(last_names,vals):
+	if key in faculty_dict:
+		faculty_dict[key].append(val)
+	else:
+		faculty_dict[key] = [val]
+
+print faculty_dict
+
+# faculty_dict = {key:value for key,value in zip(last_names,vals)}
+# print faculty_dict
